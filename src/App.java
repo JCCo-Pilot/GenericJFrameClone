2634a5546d647a763cb613644a86e14bf8b1709e
Jasper Chen <jdchen2017@gmail.com>
2024-06-14 11:24:50 -0500

    first commit

---
+++ src/App.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import javax.swing.*;
+import static java.lang.System.*;
+public class App extends JFrame{
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
