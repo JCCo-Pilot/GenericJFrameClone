9f647cef905e84ef0f490b6ca30cbd13458ce772
Jasper Chen <jdchen2017@gmail.com>
2024-06-14 11:26:12 -0500

    yeah

--- src/App.java
+++ src/App.java
@@ -5,4 +5,10 @@ public class App extends JFrame{
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
+    private final Integer xSize = 1600;
+    private final Integer ySize = 900;
+    public App (){
+        super();
+        this.setSize(xSize, ySize);
+    }
 }
