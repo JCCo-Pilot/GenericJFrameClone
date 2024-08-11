7cf50fa49a1673675838d9103dfac56a95fa5b00
Jasper Chen <jdchen2017@gmail.com>
2024-08-11 14:57:01 -0500

    yes

--- src/Practice.java
+++ src/Practice.java
@@ -1,6 +1,9 @@
 import java.util.*;
+import static java.lang.System.*;
 public class Practice {
     public static void main (String[] args){
-        
+        ArrayList<Integer> stupid = new ArrayList<>();
+        for (int i =0;i<10;i++){
+        }
     }
 }
